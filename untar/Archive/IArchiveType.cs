027d656e30dce321035d126a672c79553f720f94
generalsle1n <niels.schuler@web.de>
2025-09-01 17:07:05 +0200

    Add default method to get FolderName

--- untar/Archive/IArchiveType.cs
+++ untar/Archive/IArchiveType.cs
@@ -6,6 +6,11 @@ internal interface IArchiveType : IDisposable
 {
     internal Task<bool> IsArchiveTypeAsync(FileInfo fileInfo);
     internal Task<bool> DecompressArchiveAsync(FileInfo fileInfo, DirectoryInfo directoryInfo);
+
+    internal string GetFolderName(FileInfo fileInfo)
+    {
+        return fileInfo.Name.Replace(fileInfo.Extension, "").Replace(".","_");
+    }
     internal string ArchiveExtensions { get; }
     internal ArchiveType Type { get; }
 }
